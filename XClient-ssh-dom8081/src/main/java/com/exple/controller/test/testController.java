9946333d69a44a2d17855626cfb5b10c1644140a
chx <1294680626@qq.com>
2022-03-20 14:52:57 +0800

    add controller test http and config port 8081

---
+++ XClient-ssh-dom8081/src/main/java/com/exple/controller/test/testController.java
@@ -0,0 +1,14 @@
+package com.exple.controller.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class testController {
+
+    @GetMapping("/test.do")
+    public String testDo()
+    {
+        return "Web Spring Boot Start Success!";
+    }
+}
--- XClient-ssh-dom8081/src/main/java/com/exple/xclientsshdom/XClientSshDom8081Application.java
+++ XClient-ssh-dom8081/src/main/java/com/exple/xclientsshdom/XClientSshDom8081Application.java
@@ -2,8 +2,10 @@ package com.exple.xclientsshdom;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan({"com.exple.controller","com.exple.dao","com.exple.pojo","com.exple.service"})
 public class XClientSshDom8081Application {
 
     public static void main(String[] args) {

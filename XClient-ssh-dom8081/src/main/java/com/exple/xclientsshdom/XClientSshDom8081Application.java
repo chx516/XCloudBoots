8fda633c88ebedc999799292256ce076a4e895c7
chx <1294680626@qq.com>
2022-03-20 14:13:26 +0800

    上传项目 1.0.0

---
+++ XClient-ssh-dom8081/src/main/java/com/exple/xclientsshdom/XClientSshDom8081Application.java
@@ -0,0 +1,13 @@
+package com.exple.xclientsshdom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XClientSshDom8081Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XClientSshDom8081Application.class, args);
+    }
+
+}
---
+++ XClient-ssh-dom8081/src/test/java/com/exple/xclientsshdom/XClientSshDom8081ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exple.xclientsshdom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XClientSshDom8081ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
